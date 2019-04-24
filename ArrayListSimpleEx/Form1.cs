b9707e287dc1a080852d8545ba9d04d06c144526
tiagopontes <49887066+tiagopontes@users.noreply.github.com>
2019-04-24 14:24:31 -0300

    Finished
    App working fine

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,62 @@ namespace ArrayListSimpleEx
             InitializeComponent();
         }
 
-       // test test test test
+        ArrayList msg = new ArrayList();
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+
+            msg.Clear();
+
+            msg.Add("I");
+            msg.Add("Love");
+            msg.Add("Programming");
+            msg.Add("So");
+            msg.Add("Much");
+
+            foreach (object item in msg)
+            {
+                lblMessage.Text += item + " ";
+            }
+
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+
+            msg.Reverse();
+
+            foreach (object item in msg)
+            {
+                lblMessage.Text += item + " ";
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+
+            msg.Insert(1, txtSecondPos.Text.Trim());
+
+            foreach (object item in msg)
+            {
+                lblMessage.Text += item + " ";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+
+            msg.RemoveAt(1);
+
+            foreach (object item in msg)
+            {
+                lblMessage.Text += item + " ";
+            }
+        }
     }
 }
